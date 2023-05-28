5a99e1d5b765c47e0f8bdcc43a072226771417c7
marianaczar <progmarianasc@gmail.com>
2023-05-28 13:28:13 -0300

    deleting files

--- calculator.py
+++
@@ -1,9 +0,0 @@
-def calc(num1, num2, operator='+'):
-    if operator =='+':
-        return num1 + num2
-    elif operator == '-':
-        return num1 - num2
-    
-result = calc(10, 20)
-
-print (result)
